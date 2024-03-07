83d51645c402ca5c00bf80a3f2f6260af38f86c0
Luan Zekiri <luan.00@hotmail.de>
2024-03-07 16:34:18 +0100

    centered buttons

--- script.js
+++ script.js
@@ -1,26 +1,13 @@
-/*
-function game() {
-    let player1Choice = prompt("Your choice: ");
-
-    if (player1Choice === null) {
-        return;
-    }
-
+function playRound(choice) {
     //make input lowercase and then check if it is rock, paper or sciccos
-    let player1LowerCase = player1Choice.toLowerCase();
-
-
-    while ((player1LowerCase != "rock") && (player1LowerCase != "paper" && (player1LowerCase != "scissors"))) {
-        player1LowerCase = prompt("Only rock, paper or scissor! Your choice: ");
-        player1LowerCase = player1LowerCase.toLowerCase();
-    }
+    let player1LowerCase = choice.toLowerCase();
 
     let botChoice = getComputerChoice();
     let botChoiceLowerCase = botChoice.toLowerCase();
 
     if (player1LowerCase == botChoiceLowerCase) {
         console.log(`${botChoiceLowerCase} and ${player1LowerCase}, so it is a tie!`);
-        return; // leave method after this result
+        return;
     }
 
     if (player1LowerCase == "rock") {
@@ -29,8 +16,19 @@ function game() {
         } else {
             console.log("Player 1(you) wins, rock beats scissors!");
         }
+    } else if (player1LowerCase == "scissors") {
+        if (botChoiceLowerCase == "paper") {
+            console.log("Player 1(you) win, scissors beats paper");
+        } else {
+            console.log("Player 2(bot) wins, rock beats scissors!");
+        }
+    } else {
+        if(botChoiceLowerCase == "scissors") {
+            console.log("Player 2(bot) won, scissors beats paper!")
+        } else {
+            console.log("Playe 1(you) win, paper beats rock!")
+        }
     }
-
     return;
 }
 
@@ -43,16 +41,25 @@ function getComputerChoice() {
 }
 
 
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        game();
-    }
-}
-//execute game 5 times
-playGame();
-*/
 
 
+let playerPoints = 0;
+let computerPoints = 0;
 
+
+//add eventlistener to 3 buttons to detect which option was choosed
+let rockButton = document.querySelector(".rock")
+let paperButton = document.querySelector(".paper")
+let scissorsButton = document.querySelector(".scissors")
+
+rockButton.addEventListener("click", function () {
+    playRound("Rock");
+});
+paperButton.addEventListener("click", function () {
+    playRound("Paper");
+});
+scissorsButton.addEventListener("click", function () {
+    playRound("Scissors");
+});
 
 
